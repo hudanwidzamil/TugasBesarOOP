39d0f7a1d3656d765001c59d8b4f9d9cf5010675
vivo2419 <historia.magistra.id@gmail.com>
2020-03-31 12:30:38 +0700

    add intro and outro in main

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,49 @@ public class Main {
             System.out.println("\b") ;
         }
     }
+    public final static void introscreen(){
+        System.out.println("+----------------------------------------------------------------+");
+        System.out.println("------___________--------------------------________________-------");
+        System.out.println("-----|    ___    |------------------------|__________      |------");
+        System.out.println("-----|   |   |   |-----------------------------------|    |-------");
+        System.out.println("-----|   |___|   |---------------------------------_|   _|--------");
+        System.out.println("-----|    _______|------------------------------__|  __|----------");
+        System.out.println("-----|   |--------------____-----____--------__|  __|-------------");
+        System.out.println("-----|   |--------------|  |_____|  |------_|  __|----------------");
+        System.out.println("-----|   |----------------|  |_|  |-------|   |____________-------");
+        System.out.println("-----|___|-----------------|_____|--------|________________|------");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("+-----------------------------------------------------------------+");
+        System.out.println("                             2020                                 ");
+        
+    }
+    public final static void outroscreen(){
+        System.out.println("+----------------------------------------------------------------+");
+        System.out.println("                           THE END                                ");
+        System.out.println();
+        System.out.println("                  Thank you for playing this                      ");
+        System.out.println("                      Quarantine Content™                         ");
+        System.out.println("                            ©2020                                 ");
+        
+    }
+
     public static void main(String[] args) {
         boolean end = false;
 
         Scanner in = new Scanner(System.in);
-
+        
+        introscreen();
         System.out.println("Welcome to PvZ!");
         System.out.println("Type 'exit' to quit.");
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+        clearscreen();
 
         Player p = new Player();
 
@@ -34,7 +70,14 @@ public class Main {
                 System.out.println("position: x[1,9], y[1,4]");
                 System.out.println("input: <type> <x pos> <y pos>");
                 p.buyPlant(in.nextInt(), in.nextInt()-1, in.nextInt()-1);
-                
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch(InterruptedException ex)
+                {
+                    Thread.currentThread().interrupt();
+                }
             }
             else if (command.equals("skip")) {
                 p.skip();
@@ -46,16 +89,9 @@ public class Main {
             }
             
             p.addTurn();
-            try
-            {
-                Thread.sleep(1000);
-            }
-            catch(InterruptedException ex)
-            {
-                Thread.currentThread().interrupt();
-            }
             clearscreen();
         }
+        outroscreen();
     }
     
 }
