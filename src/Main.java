6967f792a59ca32b2eded3c1810ae6c0ef27ed34
aureliacarissaa <aureliacarissaa@gmail.com>
2020-03-31 07:13:54 +0700

    make simple main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
-        
+        boolean end = false;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Welcome to PvZ!");
+        System.out.println("Type 'exit' to quit.");
+
+        Player p = new Player();
+
+        while (! end) {
+            String command = in.next();
+            if (command.equals("exit")) {
+                end = true;
+            }
+            else if (command.equals("show")) {
+                p.showMap();
+            }
+            else if (command.equals("buy")) {
+                p.buyPlant(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            else if (command.equals("skip")) {
+                p.skip();
+            }
+        }
     }
     
 }
