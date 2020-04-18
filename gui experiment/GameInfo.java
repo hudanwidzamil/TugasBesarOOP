97aa4e064f6f5066d12b750216875e19dd9da717
vivo2419 <historia.magistra.id@gmail.com>
2020-04-18 16:54:49 +0700

    fix sunflower points threading

--- gui experiment/GameInfo.java
+++ gui experiment/GameInfo.java
@@ -14,15 +14,15 @@ public class GameInfo extends JPanel implements Runnable{
         add(new JLabel(new ImageIcon("images\\sun.png")));
         add(sunflower);
         setPreferredSize(new Dimension(100,100));
+        setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
         setOpaque(false);
     }
     public void run() {
         while (true) {
             try{
-                Thread.sleep(2000);
+                Thread.sleep(3000);
                 sfpts+=10;  
-                sunflower.setText(Integer.toBinaryString(sfpts));
-                System.out.println(sfpts);
+                sunflower.setText(Integer.toString(sfpts));
             }catch(InterruptedException ex){
                 Thread.currentThread().interrupt();
             }
